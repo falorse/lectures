4cd0341606aa14981a7e3b1ce5fde29a0489c5f5
fukuda01112 <fukuda-shota656@g.ecc.u-tokyo.ac.jp>
2017-12-07 01:50:23 +0900

    refactoring init

--- softwareDevelopmentExercise/multiply_matrix.c
+++ softwareDevelopmentExercise/multiply_matrix.c
@@ -10,9 +10,11 @@
 #define OUT 0
 //#define NO_MAT_FILE
 
+void get_file_paths(char* A_path, char* B_path, char* C_path, char** argv);
+bool is_input_matrixes_invalid(struct Matrix* A, struct Matrix* B);
 void create_global_matrixes(struct Matrix* A, char* A_path, struct Matrix* B,
         char* B_path, struct Matrix* transeposed_B, struct Matrix* C, int myid);
-void create_local_matrixes(struct Matrix* local_A, struct Matrix* local_C, 
+void create_local_matrixes(struct Matrix* local_A, struct Matrix* local_C,
         int a_rows, int a_cols, int b_cols, int myid, int procs_num);
 void communicate_by_mpi(struct Matrix* A, struct Matrix* local_A,
         struct Matrix* B, struct Matrix* transeposed_B);
@@ -22,38 +24,32 @@ struct Matrix* read_input_file(char* file_path, int myid);
 
 int main(int argc, char *argv[]) {
 
-    // 引数チェックとファイルパスの取得
-    char A_path[MAXLEN], B_path[MAXLEN], C_path[MAXLEN];
-    if (argc == 4) {
-        strcpy(A_path, argv[1]);
-        strcpy(B_path, argv[2]);
-        strcpy(C_path, argv[3]);
-    } else {
+    // check argc and get file paths
+    if (argc != 4) {
         printf("ERROR USAGE: mul_matrix leftMat_path rightMat_path ansMat_path\n");
         return 0;
     }
+    char A_path[MAXLEN], B_path[MAXLEN], C_path[MAXLEN];
+    get_file_paths(A_path, B_path, C_path, argv);
 
-    struct Matrix *A, *B, *C, *transeposed_B, *local_A, *local_C;
-    double start, end;
-
-    /* MPI Initialize */
+    // MPI Initialize
     int myid, procs_num;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &procs_num);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
+    double start, end;
     start = MPI_Wtime();
 
     // create matrixes
     // create A, B, C without bufs in myid != 0
+    struct Matrix *A, *B, *C, *transeposed_B, *local_A, *local_C;
     A = read_input_file(A_path, myid);
     B = read_input_file(B_path, myid);
-    
-    if(A == NULL || B == NULL || A->cols != B->rows){
-        printf("ERROR: input file is invalid");
+
+    if(is_input_matrixes_invalid(A, B))
         return 0;
-    }
-    
+
     if (myid == 0) {
         C = create_mat(A->rows, B->cols);
         transeposed_B = transepose_mat(B);
@@ -61,23 +57,29 @@ int main(int argc, char *argv[]) {
         C = create_mat_without_bufs(A->rows, B->cols);
         transeposed_B = create_mat(B->cols, B->rows);
     }
-    
+
     local_A = create_mat(A->rows / procs_num, A->cols);
     local_C = create_mat(A->rows / procs_num, B->cols);
 
-    communicate_by_mpi(A, local_A, B, transeposed_B);
+    // communicate by MPI
+    MPI_Scatter(A->bufs, get_mat_size(local_A), MPI_DOUBLE, local_A->bufs,
+            get_mat_size(local_A), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(transeposed_B->bufs, get_mat_size(transeposed_B),
+            MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    destroy_mat(A);
+    destroy_mat(B);
 
     calculate(local_A, transeposed_B, local_C);
 
-    MPI_Gather(local_C->bufs, get_mat_size(local_C), MPI_DOUBLE, 
-            get_mat_bufs(C), get_mat_size(local_C), MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    
-    // write C and output run time
+    MPI_Gather(local_C->bufs, get_mat_size(local_C), MPI_DOUBLE,
+            C->bufs, get_mat_size(local_C), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // write ans to file and output run time
     if (myid == 0) {
         write_mat_file(C_path, C);
         end = MPI_Wtime();
         printf("N:%d, time:%f, processes:%d, threads:%d\n",
-                get_mat_size(C), (end - start),procs_num, omp_get_max_threads());
+                get_mat_size(C), (end - start), procs_num, omp_get_max_threads());
     }
 
     // finalize
@@ -88,6 +90,19 @@ int main(int argc, char *argv[]) {
     MPI_Finalize();
     return 0;
 }
+
+void get_file_paths(char* A_path, char* B_path, char* C_path, char** argv) {
+    strcpy(A_path, argv[1]);
+    strcpy(B_path, argv[2]);
+    strcpy(C_path, argv[3]);
+};
+
+bool is_input_matrixes_invalid(struct Matrix* A, struct Matrix* B){
+  if(A == NULL || B == NULL || A->cols != B->rows) {
+      return true;
+  }
+  return false;
+};
 
 struct Matrix* read_input_file(char* file_path, int myid) {
     int i, j, rows, cols;
@@ -134,7 +149,7 @@ void calculate(const struct Matrix* local_A,
         const struct Matrix* transeposed_B, struct Matrix* local_C) {
     int i, j, k;
     double tmp;
-    
+
     // turning by loop unwinging and putting tmp on register
 #pragma omp parallel for private(i,k,tmp)
     for (j = 0; j < transeposed_B->rows; j += 8) {
